b11c81b9219c38ea2ed4e30880935c3069417b6e
danisuttoff <danisuttoffds@gmail.com>
2022-12-13 21:53:47 -0300

    aoTransferir.emit

--- bytebank/src/app/app.component.ts
+++ bytebank/src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank';
+  transferencia: any;
+  //destino: number;
+  //valor: number;
+
+  transferir($event){
+    console.log($event);
+    this.transferencia = $event;
+    //this.destino = $event.destino;
+    //this.valor = $event.valor;
+
+  }
 }
